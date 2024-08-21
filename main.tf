ec1393bb024818e6e619aa04f3b847f08409d12f
Jeff Chase <36856014+jeffdmipshell@users.noreply.github.com>
2024-08-21 15:41:45 -0400

    d version

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-tf_V3"
+    /* resource_group_name  = "rg-tf_V3"
     storage_account_name = "sttfv3jdca"
     container_name       = "tfstatefile"
     key                  = "terraform.tfstate"
     subscription_id      = "141950e1-13e1-447a-8ef4-38a502de49ba"
+  */
   }
-
 }
 
 provider "azurerm" {
