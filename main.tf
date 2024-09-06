7c6c1690deb2186bcd03f7e368ee695240fadfe0
Jeff Chase <jeffdchase@outlook.com>
2024-09-06 17:04:44 -0400

    asd

--- main.tf
+++ main.tf
@@ -6,12 +6,11 @@ terraform {
     }
   }
   backend "azurerm" {
-    /*resource_group_name  = "rg-tf_V3"
+    resource_group_name  = "rg-tf_V3"
     storage_account_name = "sttfv3jdca"
     container_name       = "tfstatefile"
     key                  = "terraform.tfstate"
     subscription_id      = "99efcdcb-3c41-4806-adcc-971369124351"
-*/
   }
 }
 
