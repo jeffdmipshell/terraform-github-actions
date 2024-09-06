af4c09801ef303c14646a77e7720fe8a0537e052
Jeff Chase <jeffdchase@outlook.com>
2024-09-06 14:54:35 -0400

    Medium

---
+++ .github/workflows/backend.tf
@@ -0,0 +1,11 @@
+﻿# backend.tf
+
+terraform {
+  backend "azurerm" {
+resource_group_name  = "rg-tf_V3"
+    storage_account_name = "sttfv3jdca"
+    container_name       = "tfstatefile"
+    key                  = "terraform.tfstate"
+    subscription_id      = "141950e1-13e1-447a-8ef4-38a502de49ba"
+  }
+}
